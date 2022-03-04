1d3744525c33899d0839b31b8a12287249b018f0
NikitaZhimirdeev <94803242+NikitaZhimirdeev@users.noreply.github.com>
2022-03-04 20:11:10 +0700

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-folder_ID = '1KF_fIxG9WCy_Vh2zun-BNJlESG1lKVFp'
-msg_From = 'aleksandrprojects4@gmail.com'
-password = 'Scm1012-21'
-msg_To = 'zhimirdeevrabota@gmail.com'
-Token='1913610401:AAGWsUP31hXLSYWHp5mCtAX3aJMkNzoP2NI'
-chat_id=657572123
